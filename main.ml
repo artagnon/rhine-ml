f1c3251ab654891b2d1653a29cac48a07ab94fed
Ramkumar Ramachandra <artagnon@gmail.com>
2014-07-24 19:26:35 -0400

    main: re-indent with indent-tabs-mode nil

--- main.ml
+++ main.ml
@@ -3,46 +3,45 @@ type action = Debug | Normal
 (* pretty print atoms *)
 let ppatom p a = match p with
     Symbol(s) -> (match a with
-		    Debug -> print_string ("sym:" ^ s)
-		   |Normal -> print_string s)
+                    Debug -> print_string ("sym:" ^ s)
+                   |Normal -> print_string s)
    |Int(i) -> (match a with
-		 Debug -> print_string "int:"; print_int i
-		|Normal -> print_int i)
+                 Debug -> print_string "int:"; print_int i
+                |Normal -> print_int i)
    |Double(d) -> (match a with
-		    Debug -> print_string "dbl:"; print_float 
-						    d
-		   |Normal -> print_float d)
+                    Debug -> print_string "dbl:"; print_float d
+                   |Normal -> print_float d)
    |Nil -> (match a with
-	      Debug -> print_string "sym:nil"
-	     |Normal -> print_string "nil")
-	     
+              Debug -> print_string "sym:nil"
+             |Normal -> print_string "nil")
+
 (* pretty print S-expressions *)
 let rec ppsexpr p islist act = match p with
     Atom(a) -> ppatom a act
    |DottedPair(se1,se2) -> if islist then () else 
-			     print_string "( ";
-			   (match se1 with
-			      Atom(a) -> ppatom a act
-			     |_ -> ppsexpr se1 false act);
-			   print_char ' ';
-			   (match se2 with
-			      Atom(Nil) -> print_char ')'
-			     |Atom(_ as a) -> print_string ". ";
-					      ppatom a act; 
-					      print_string " )"
-			     |_ -> ppsexpr se2 true act)
+                             print_string "( ";
+                           (match se1 with
+                              Atom(a) -> ppatom a act
+                             |_ -> ppsexpr se1 false act);
+                           print_char ' ';
+                           (match se2 with
+                              Atom(Nil) -> print_char ')'
+                             |Atom(_ as a) -> print_string ". ";
+                                              ppatom a act;
+                                              print_string " )"
+                             |_ -> ppsexpr se2 true act)
 (* pretty print the program *)
 let pprint p a = match p with
     Prog(ss) -> List.iter (fun i -> ppsexpr i false a;
-				    print_newline ();
-				    print_newline ()) ss
+                                    print_newline ();
+                                    print_newline ()) ss
 
 (* starting point *)
 let _ =
   let action = 
     if Array.length Sys.argv > 1 then
       try
-	List.assoc Sys.argv.(1) [ ("-d", Debug) ]
+        List.assoc Sys.argv.(1) [ ("-d", Debug) ]
       with Not_found -> Normal
     else
       Normal in
