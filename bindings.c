e5159d6dba49812aed0f115ce43310c14fbe0f74
Ramkumar Ramachandra <artagnon@gmail.com>
2014-08-10 11:58:16 -0400

    bindings, sample3: get println to print different types

--- bindings.c
+++ bindings.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 struct value_t {
-	int type;
+	int type_tag;
 	long int_val;
 	int bool_val:1;
 	char *string_val;
@@ -11,7 +11,20 @@ struct value_t {
 
 extern struct value_t *println(struct value_t *v) {
 	struct value_t *ret;
-	printf("Hello! %ld\n", v->int_val);
+	switch(v->type_tag) {
+	case 1:
+		printf("(int) %ld\n", v->int_val);
+		break;
+	case 2:
+		printf("(bool) %s\n", !v->bool_val ? "false" : "true");
+		break;
+	case 3:
+		printf("(string) %s\n", v->string_val);
+		break;
+	default:
+		printf("Don't know how to print type %d\n", v->type_tag);
+		exit(1);
+	}
 	ret = malloc(sizeof(struct value_t));
 	ret->int_val = 5;
 	return ret;
