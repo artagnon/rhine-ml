870e56cfbb04d8e14dcfd4f5bdbd32ce7d6b5b3c
Ramkumar Ramachandra <artagnon@gmail.com>
2014-08-13 16:06:30 -0400

    bindings: whitespace

--- bindings.c
+++ bindings.c
@@ -57,169 +57,156 @@ extern struct value_t *println(struct value_t *v) {
 }
 
 double get_value(struct value_t *v, int *ret_type) {
-    if (v->type_tag == 6) {
-        *ret_type = 6;
-        return v->dbl_val;
-    }
-    else {
-        return (double) v->int_val;
-    }
+	if (v->type_tag == 6) {
+		*ret_type = 6;
+		return v->dbl_val;
+	} else {
+		return (double) v->int_val;
+	}
 }
 
 struct value_t *save_value(double val, int ret_type) {
-    struct value_t *ret;
-    ret = malloc(sizeof(struct value_t));
+	struct value_t *ret;
+	ret = malloc(sizeof(struct value_t));
 
-    if (ret_type == 6) {
-        ret->dbl_val = val;
-        ret->type_tag = 6;
-    }
-    else if (ret_type == 2) {
-        ret->bool_val = (int)val;
-        ret->type_tag = 2;
-    }
-    else {
-        ret->int_val = (int) val;
-        ret->type_tag = 1;
-    }
-    return ret;
+	if (ret_type == 6) {
+		ret->dbl_val = val;
+		ret->type_tag = 6;
+	} else if (ret_type == 2) {
+		ret->bool_val = (int)val;
+		ret->type_tag = 2;
+	} else {
+		ret->int_val = (int) val;
+		ret->type_tag = 1;
+	}
+	return ret;
 }
 
 extern struct value_t *cadd(struct value_t *v, struct value_t *v2) {
-    int ret_type = 0;
-    double val = get_value(v, &ret_type) + get_value(v2, &ret_type);
-    struct value_t *ret = save_value(val, ret_type);
-    return ret;
+	int ret_type = 0;
+	double val = get_value(v, &ret_type) + get_value(v2, &ret_type);
+	struct value_t *ret = save_value(val, ret_type);
+	return ret;
 }
 
 extern struct value_t *cdiv(struct value_t *v, struct value_t *v2) {
-    int ret_type = 6; // force float
-    double val = get_value(v, &ret_type) / get_value(v2, &ret_type);
-    struct value_t *ret = save_value(val, ret_type);
-    return ret;
+	int ret_type = 6; // force float
+	double val = get_value(v, &ret_type) / get_value(v2, &ret_type);
+	struct value_t *ret = save_value(val, ret_type);
+	return ret;
 }
 
 extern struct value_t *csub(struct value_t *v, struct value_t *v2) {
-    int ret_type = 0;
-    double val = get_value(v, &ret_type) - get_value(v2, &ret_type);
-    struct value_t *ret = save_value(val, ret_type);
-    return ret;
+	int ret_type = 0;
+	double val = get_value(v, &ret_type) - get_value(v2, &ret_type);
+	struct value_t *ret = save_value(val, ret_type);
+	return ret;
 }
 
 extern struct value_t *cmul(struct value_t *v, struct value_t *v2) {
-    int ret_type = 0;
-    double val = get_value(v, &ret_type) * get_value(v2, &ret_type);
-    struct value_t *ret = save_value(val, ret_type);
-    return ret;
+	int ret_type = 0;
+	double val = get_value(v, &ret_type) * get_value(v2, &ret_type);
+	struct value_t *ret = save_value(val, ret_type);
+	return ret;
 }
 
 extern struct value_t *clt(struct value_t *v, struct value_t *v2) {
-    int ret_type = 0;
-    struct value_t *ret;
-    if (get_value(v, &ret_type) < get_value(v2, &ret_type)) {
-        ret = save_value(1.0, 2);
-    } else {
-        ret = save_value(0.0, 2);
-    }
-    return ret;
+	int ret_type = 0;
+	struct value_t *ret;
+	if (get_value(v, &ret_type) < get_value(v2, &ret_type)) {
+		ret = save_value(1.0, 2);
+	} else {
+		ret = save_value(0.0, 2);
+	}
+	return ret;
 }
 
 extern struct value_t *cgt(struct value_t *v, struct value_t *v2) {
-    int ret_type = 0;
-    struct value_t *ret;
-    if (get_value(v, &ret_type) > get_value(v2, &ret_type)) {
-        ret = save_value(1.0, 2);
-    } else {
-        ret = save_value(0.0, 2);
-    }
-    return ret;
+	int ret_type = 0;
+	struct value_t *ret;
+	if (get_value(v, &ret_type) > get_value(v2, &ret_type)) {
+		ret = save_value(1.0, 2);
+	} else {
+		ret = save_value(0.0, 2);
+	}
+	return ret;
 }
 
 extern struct value_t *clte(struct value_t *v, struct value_t *v2) {
-    int ret_type = 0;
-    struct value_t *ret;
-    if (get_value(v, &ret_type) <= get_value(v2, &ret_type)) {
-        ret = save_value(1.0, 2);
-    } else {
-        ret = save_value(0.0, 2);
-    }
-    return ret;
+	int ret_type = 0;
+	struct value_t *ret;
+	if (get_value(v, &ret_type) <= get_value(v2, &ret_type)) {
+		ret = save_value(1.0, 2);
+	} else {
+		ret = save_value(0.0, 2);
+	}
+	return ret;
 }
 
 extern struct value_t *cgte(struct value_t *v, struct value_t *v2) {
-    int ret_type = 0;
-    struct value_t *ret;
-    if (get_value(v, &ret_type) >= get_value(v2, &ret_type)) {
-        ret = save_value(1.0, 2);
-    } else {
-        ret = save_value(0.0, 2);
-    }
-    return ret;
+	int ret_type = 0;
+	struct value_t *ret;
+	if (get_value(v, &ret_type) >= get_value(v2, &ret_type)) {
+		ret = save_value(1.0, 2);
+	} else {
+		ret = save_value(0.0, 2);
+	}
+	return ret;
 }
 
 extern struct value_t *cequ(struct value_t *v, struct value_t *v2) {
-    int ret_type = 0;
-    struct value_t *ret;
-    // only makes sense for integers and bools currently
-    switch(v->type_tag) {
-    case 1:
-        if (v->int_val == v2->int_val) {
-            ret = save_value(1.0, 2);
-        } else {
-            ret = save_value(0.0, 2);
-        }
-        break;
-    case 2:
-        if (!v->bool_val == !v2->bool_val) {
-            ret = save_value(1.0, 2);
-        } else {
-            ret = save_value(0.0, 2);
-        }
-        break;
-    }
-    return ret;
+	int ret_type = 0;
+	struct value_t *ret;
+	// only makes sense for integers and bools currently
+	switch(v->type_tag) {
+	case 1:
+		if (v->int_val == v2->int_val) {
+			ret = save_value(1.0, 2);
+		} else {
+			ret = save_value(0.0, 2);
+		}
+		break;
+	case 2:
+		if (!v->bool_val == !v2->bool_val) {
+			ret = save_value(1.0, 2);
+		} else {
+			ret = save_value(0.0, 2);
+		}
+		break;
+	}
+	return ret;
 }
 
 extern struct value_t *cand(struct value_t *v, struct value_t *v2) {
-    int ret_type = 0;
-    struct value_t *ret;
-    if (!(!v->bool_val) && !(!v2->bool_val)) {
-        ret = save_value(1.0, 2);
-    } else {
-        ret = save_value(0.0, 2);
-    }
-    return ret;
+	int ret_type = 0;
+	struct value_t *ret;
+	if (!(!v->bool_val) && !(!v2->bool_val)) {
+		ret = save_value(1.0, 2);
+	} else {
+		ret = save_value(0.0, 2);
+	}
+	return ret;
 }
 
 extern struct value_t *cor(struct value_t *v, struct value_t *v2) {
-    int ret_type = 0;
-    struct value_t *ret;
-    if (!(!v->bool_val) || !(!v2->bool_val)) {
-        ret = save_value(1.0, 2);
-    } else {
-        ret = save_value(0.0, 2);
-    }
-    return ret;
+	int ret_type = 0;
+	struct value_t *ret;
+	if (!(!v->bool_val) || !(!v2->bool_val)) {
+		ret = save_value(1.0, 2);
+	} else {
+		ret = save_value(0.0, 2);
+	}
+	return ret;
 
 }
 
 extern struct value_t *cnot(struct value_t *v) {
-    int ret_type = 0;
-    struct value_t *ret;
-    if (!(!v->bool_val)) {
-        ret = save_value(0.0, 2);
-    } else {
-        ret = save_value(1.0, 2);
-    }
-    return ret;
-
+	int ret_type = 0;
+	struct value_t *ret;
+	if (!(!v->bool_val)) {
+		ret = save_value(0.0, 2);
+	} else {
+		ret = save_value(1.0, 2);
+	}
+	return ret;
 }
-
-
-
-
-
-
-
-
-
