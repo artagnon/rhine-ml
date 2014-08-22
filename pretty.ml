6da841abfe87591df7cb7892a400b686ad80156e
Ramkumar Ramachandra <artagnon@gmail.com>
2014-08-22 19:56:54 -0400

    pretty: refactor to print strings, not stdout directly
    We need this for sane error reporting.

--- pretty.ml
+++ pretty.ml
@@ -1,37 +1,35 @@
+open Format
 open Ast
 
 exception Error of string
 
-let print_bool = function true -> print_string "true"
-                        | false -> print_string "false"
+let sprint_bool = function true -> "true"
+                         | false -> "false"
 
 (* pretty print atoms *)
 let ppatom p = match p with
-    Symbol(s) -> print_string ("sym:" ^ s)
-  | String(s) -> print_string ("str:" ^ s)
-  | Int(i) -> print_string "int:"; print_int i
-  | Char(i) -> print_string "char:"; print_char i
-  | Bool(i) -> print_string "bool:"; print_bool i
-  | Double(d) -> print_string "dbl:"; print_float d
-  | Nil -> print_string "nil"
+    Symbol(s) -> "sym:" ^ s
+  | String(s) -> "str:" ^ s
+  | Int(i) -> sprintf "int:%d" i
+  | Char(i) -> sprintf "char:%c" i
+  | Bool(i) -> sprintf "bool:%s" (sprint_bool i)
+  | Double(d) -> sprintf "dbl:%f" d
+  | Nil -> "nil"
 
 (* pretty print S-expressions *)
-let rec ppsexpr p = match p with
+let rec ppsexpr p =
+  let lfolder i j = i ^ (match j with
+                           Atom(a) -> ppatom a ^ " "
+                         | _ -> ppsexpr j) in
+  let vfolder i j = i ^ ppsexpr j ^ " " in
+  match p with
     Atom(a) -> ppatom a
-  | List(sel) -> print_string "( ";
-                 List.iter (fun i -> match i with
-                                       Atom(a) -> ppatom a;
-                                                  print_char ' '
-                                     | _ -> ppsexpr i) sel;
-                 print_string " )"
-  | Vector(qs) -> print_string "[ ";
-                    List.iter (fun i -> ppsexpr i;
-                                        print_char ' ') qs;
-                    print_char ']'
+  | List(sel) -> sprintf "(%s)" (List.fold_left lfolder "" sel)
+  | Vector(qs) -> sprintf "[%s]" (List.fold_left vfolder "" qs)
   | _ -> raise (Error "Don't know how to print cooked AST")
 
 (* pretty print the program *)
 let pprint p = match p with
-    Prog(ss) -> List.iter (fun i -> ppsexpr i;
+    Prog(ss) -> List.iter (fun i -> print_string (ppsexpr i);
                                     print_newline ();
                                     print_newline ()) ss
