6d5d1c050cd3d125e0b3e61e213d37cc31a64dbc
Ramkumar Ramachandra <artagnon@gmail.com>
2014-08-24 22:17:24 -0400

    toplevel: declare va_start and va_end intrinsics

--- toplevel.ml
+++ toplevel.ml
@@ -102,6 +102,10 @@ let main_loop ss =
                          [| pointer_type i8_type; pointer_type i8_type;
                             i64_type; i32_type; i1_type |] in
   ignore (declare_function "llvm.memcpy.p0i8.p0i8.i64" ft the_module);
+  let ft = function_type i32_type [| pointer_type i8_type |] in
+  ignore (declare_function "llvm.va_start" ft the_module);
+  let ft = function_type i32_type [| pointer_type i8_type |] in
+  ignore (declare_function "llvm.va_end" ft the_module);
   ignore (codegen_proto (Ast.Prototype("println", Array.make 1 "v")));
   ignore (codegen_proto (Ast.Prototype("print", Array.make 1 "v")));
   ignore (codegen_proto (Ast.Prototype("cadd", Array.make 2 "v")));
