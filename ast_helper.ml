4a5156c2c181a612c0a51c3dd8744f0d6ef8a4be
Ramkumar Ramachandra <artagnon@gmail.com>
2015-03-24 18:55:41 -0400

    Flush out a lot of OCaml code

--- ast_helper.ml
+++
@@ -1,7 +0,0 @@
-open Parsetree
-
-let default_loc = ref Location.none;;
-
-module Lsexpr = struct
-  let mk ?(loc = !default_loc) d = { lsexpr_desc = d; lsexpr_loc = loc }
-end
--- debug.sh
+++
@@ -1,1 +0,0 @@
-OCAMLRUNPARAM='p' ./rhine
